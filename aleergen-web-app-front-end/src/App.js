97bf42e283d3d08776a5ae123501119b1dcd31d5
Miguel-Graham <grahammiguel17@yahoo.com>
2023-09-03 13:59:44 -0400

    commit

---
+++ aleergen-web-app-front-end/src/App.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import SearchBar from './SearchBar';
+import './index.css';
+
+function App() {
+  const [menu, setMenu] = useState([]);
+  const [allergies, setAllergies] = useState([]);
+
+  // fetch menu from API and update state
+  useEffect(() => {
+    fetch('/api/menu')
+        .then(res => res.json())
+        .then(data => setMenu(data));
+  }, []);
+
+  // filter menu items based on selected allergies
+  const filteredMenu = menu.filter(item => {
+    for (let i = 0; i < allergies.length; i++) {
+      if (!item.allergies.includes(allergies[i])) {
+        return false;
+      }
+    }
+    return true;
+  });
+
+  return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            Restaurant Menu
+          </a>
+          <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Menu
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <h1></h1>
+        <SearchBar setMenu={setMenu} />
+        <ul>
+          {filteredMenu.map(item => (
+              <li key={item.id}>
+                {item.name} - {item.price}
+              </li>
+          ))}
+        </ul>
+      </div>
+  );
+}
+
+export default App;
---
+++ aleergen-web-app-front-end/src/SearchBar.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./SearchBar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const SearchBar = ({ setMenu }) => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [selectedAllergy, setSelectedAllergy] = useState("");
+    const [response, setResponse] = useState("");
+
+    const handleSearch = async () => {
+        try {
+            const response = await axios.get("http://localhost:8080/allergy/getAllergy", {
+                params: {
+                    dishName: searchTerm,
+                    allergyName: selectedAllergy,
+                },
+            });
+
+            // Do something with the response data
+            setMenu(response.data);
+            setResponse(response.data);
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleSelectChange = (event) => {
+        setSelectedAllergy(event.target.value);
+    };
+
+    return (
+        <div className="search-container">
+            <input
+                type="text"
+                placeholder="Search for a dish"
+                value={searchTerm}
+                onChange={(event) => setSearchTerm(event.target.value)}
+                className="form-control"
+            />
+            <select
+                className="form-control mt-2 dropdown-select"
+                value={selectedAllergy}
+                onChange={handleSelectChange}
+            >
+                <option value="">Select Allergy</option>
+                <option value="peanuts">Peanuts</option>
+                <option value="dairy">Dairy</option>
+                <option value="gluten">Gluten</option>
+                <option value="Nuts">Nuts</option>
+                <option value="shellfish">Shellfish</option>
+
+            </select>
+            <button className="btn btn-primary mt-2" onClick={handleSearch}>
+                Search
+            </button>
+            {response && (
+                <div className="response-container mt-2">
+                    <h5>Response:</h5>
+                    <pre>{JSON.stringify(response, null, 2)}</pre>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default SearchBar;
