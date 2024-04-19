d593df43096345baa4a8d802248fbe2b1b5b3f04
Miguel-Graham <grahammiguel17@yahoo.com>
2024-04-19 10:06:18 -0400

    Addressed errors

--- aleergen-web-app-front-end/src/App.js
+++ aleergen-web-app-front-end/src/App.js
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import SearchBar from './SearchBar.js';
 import Navbar from './Navbar.js';
 import './index.css';
 
 function App() {
-  const [menu, setMenu] = useState([]);
-  const [allergies] = useState([]);
-
-  // filter menu items based on selected allergies
+  const [setMenu] = useState([]);
+// filter menu items based on selected allergies
 
   return (
       <div>
